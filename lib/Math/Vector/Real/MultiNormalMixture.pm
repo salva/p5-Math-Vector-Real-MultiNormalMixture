1054693c7d986ea6bb6936830213b7e08e1598d3
Salvador <sfandino@yahoo.com>
2011-06-20 19:00:04 +0200

    working on it

--- lib/Math/Vector/Real/MultiNormalMixture.pm
+++ lib/Math/Vector/Real/MultiNormalMixture.pm
@@ -1,76 +1,195 @@
 package Math::Vector::Real::MultiNormalMixture;
 
-use 5.012003;
-use strict;
-use warnings;
-
-require Exporter;
-
-our @ISA = qw(Exporter);
-
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
-
-# This allows declaration	use Math::Vector::Real::MultiNormalMixture ':all';
-# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
-# will save memory.
-our %EXPORT_TAGS = ( 'all' => [ qw(
-	
-) ] );
-
-our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
-
-our @EXPORT = qw(
-	
-);
-
 our $VERSION = '0.01';
 
+use 5.010;
+use strict;
+use warnings;
+use Carp;
 
-# Preloaded methods go here.
+use Math::Vector::Real;
+
+my $PI = 3.14159265358979323846264338327950288419716939937510;
+
+sub new {
+    my ($class, %opts) = @_;
+    my $mu = delete $opts{mu} // croak "required argument 'mu' missing";
+    @$mu >= 1 or die "'mu' must containt one point at least";
+    my $dim = @{$mu->[0]};
+    my $n = @$mu;
+    my $alpha = delete $opts{alpha};
+    my @alpha = (defined $alpha ? @$alpha : ((1/$n) x $n));
+    my $sigma = delete $opts{sigma} // 1.0;
+    my @sigma = (ref $sigma ? @$sigma : (($sigma) x $n));
+    croak "bad array size" unless (@alpha == $n and @sigma == $n);
+    my $c1 = (2 * $PI) ** (-0.5 * $dim);
+    my @c = map $alpha[$_] * $c1 * ($sigma[$_] ** (-$dim)), (0..$#alpha);
+    my @isigma2 = map 1.0/($_ * $_), @sigma;
+    my $c2 = -sqrt(2.0 / $PI)
+    my @second = map $c2/($_ * $_ * $_);
+
+    my $self = { mu => [map Math::Vector::Real::clone($_), @$mu],
+                 alpha => \@alpha,
+                 sigma => \@sigma,
+                 isigma2 => \@isigma2,
+                 c => \@c,
+                 second => \@second,
+                 dim => $dim,
+               };
+    bless $self, $class;
+}
+
+sub density {
+    my ($self, $p) = @_;
+    my $c = $self->{c};
+    my $mu = $self->{mu};
+    my $isigma2 = $self->{sigma};
+    my $acu = 0;
+    for (0..$#$mu) {
+        $acu += $c->[$_] * exp($isigma2->[$_] * $mu->[$_]->dist2($p));
+    }
+    $acu;
+}
+
+sub density_and_gradient {
+    my ($self, $p) = @_;
+    my $c = $self->{c};
+    my $mu = $self->{mu};
+    my $isigma2 = $self->{sigma};
+    my $d = 0;
+    my $g = $Math::Vector::Real->zero(scalar @{$mu->[0]});
+    for (0..$#mu) {
+        my $mu_p = $p - $mu->[$_];
+        my $isigma2 = $isigma2->[$_];
+        my $Dd = $c->[$_] * exp($isigma2 * $mu_p->abs2);
+        $d += $Dd;
+        $g += -2 * $isigma2 * $mu_p * $Dd;
+    }
+    return ($d, $g);
+}
+
+sub max_density_estimation {
+    my $self = shift;
+    my $mu = $self->{mu};
+    my $max = $self->density($mu->[0]);
+    for my $ix (1..$#$mu) {
+        my ($maxd, $g) = $self->density($mu->[$ix]);
+        $max = $d if $d > $max;
+    }
+    return $max;
+}
 
 1;
 __END__
-# Below is stub documentation for your module. You'd better edit it!
 
 =head1 NAME
 
-Math::Vector::Real::MultiNormalMixture - Perl extension for blah blah blah
+Math::Vector::Real::MultiNormalMixture - Multinormal Mixture distribution
 
 =head1 SYNOPSIS
 
   use Math::Vector::Real::MultiNormalMixture;
-  blah blah blah
+
+  my $mnm = Math::Vector::Real::MultiNormalMixture->new(
+       mu    => [[0.0, 0.0], [1.0, 0.0], [0.0, 1.5]],
+       sigma => [ 1.0,        1.0,        2.0      ],
+       alpha => [ 0.5,        0.25,       0.25     ]
+  );
+
+  my $d = $mnm->density([0.5, 0.2]);
 
 =head1 DESCRIPTION
 
-Stub documentation for Math::Vector::Real::MultiNormalMixture, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
+This module allows to calculate the density of a mixture of n
+multivariate normal simetric distributions.
 
-Blah blah blah.
+Given a multivariate normal simetric distributions in IR**k (IR := the
+real numbers domain) such that its density function can be calculated
+as...
 
-=head2 EXPORT
+  p($x) = (sqrt(2*pi)*$sigma)**(-$k) * exp(|$x-$mu|/$sigma)**2)
 
-None by default.
+  where
+
+    $x is a vector of dimension k,
+    $mu is the median vector of the distribution,
+    $d = |$x - $mu|, the distance between the median and x
+    $sigma is the standard deviation,
+    (the covariance matrix is restricted to $sigma*$Ik being $Ik
+     the identity matrix of size k)
+
+A multivariate normal distribution mixin is defined as a weighted mix
+of a set of multivariate normal simetric distributions, such that its
+density function is...
+
+  pm(x) = sum ( $alpha[$i] * p[$i](x) )
 
 
+=head2 API
+
+The following methods are available:
+
+=over 4
+
+=item $mnm = Math::Vector::Real::MultiNormalMixture->new(%opts)
+
+Creates a new Multivariate Normal Mixture distribution object.
+
+The accepted arguments are as follow:
+
+=over 4
+
+=item mu => \@mu
+
+An array of vectors (or array references) containing the mediams of
+the single multinormal distributions.
+
+=item alpha => \@alpha
+
+An array of coeficients with the mixing weights. This argument is
+optional.
+
+=item sigma => \@sigma
+
+An array with the sigma parameter for every one of the multinormal
+distributions.
+
+A single value can also be provided and all the multinormal
+distributions will have it.
+
+The default sigma value is 1.0.
+
+=back
+
+=item $mnm->density($p)
+
+Returns the distribution density at the given point.
+
+
+=item $dd = $mnm->density_and_gradient($p)
+
+Returns density and the gradient of the density function at the given
+point.
+
+=item $d = $mnm->max_density_estimation
+
+Returns the maximun value of the density in IR**k.
+
+=back
 
 =head1 SEE ALSO
 
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
+Mixture distribution on the wikipedia:
+L<http://en.wikipedia.org/wiki/Mixture_model>
 
-If you have a mailing list set up for your module, mention it here.
+Multivariate normal distribution on the wikipedia:
+L<http://en.wikipedia.org/wiki/Multivariate_normal_distribution>.
 
-If you have a web site set up for your module, mention it here.
+L<Math::Vector::Real>
 
 =head1 AUTHOR
 
-Salvador Fandino, E<lt>salva@E<gt>
+Salvador FandiE<ntilde>o, E<lt>sfandino@yahoo.comE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
